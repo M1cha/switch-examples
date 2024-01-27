f95a415bb4fc1e240897b191827206c3a496e0ff
Michael Zimmermann <sigmaepsilon92@gmail.com>
2024-01-27 21:30:04 +0100

    hid/read-controls: show all analog sticks of all controllers

--- hid/read-controls/source/main.c
+++ hid/read-controls/source/main.c
@@ -8,6 +8,10 @@
 
 // See also libnx pad.h / hid.h.
 
+
+#define ARRAY_SIZE(array) \
+	((size_t) ((sizeof(array) / sizeof((array)[0]))))
+
 // Main program entrypoint
 int main(int argc, char* argv[])
 {
@@ -19,11 +23,14 @@ int main(int argc, char* argv[])
     consoleInit(NULL);
 
     // Configure our supported input layout: a single player with standard controller styles
-    padConfigureInput(1, HidNpadStyleSet_NpadStandard);
+    padConfigureInput(8, HidNpadStyleSet_NpadStandard);
 
     // Initialize the default gamepad (which reads handheld mode inputs as well as the first connected controller)
-    PadState pad;
-    padInitializeDefault(&pad);
+    PadState pads[8];
+
+    for(size_t i=0; i<ARRAY_SIZE(pads); i++) {
+        padInitializeWithMask(&pads[i], 1 << i);
+    }
 
     //Matrix containing the name of each key. Useful for printing when a key is pressed
     char keysNames[28][32] = {
@@ -36,8 +43,6 @@ int main(int argc, char* argv[])
         "LeftSL", "LeftSR", "RightSL", "RightSR",
     };
 
-    u32 kDownOld = 0, kHeldOld = 0, kUpOld = 0; //In these variables there will be information about keys detected in the previous frame
-
     printf("\x1b[1;1HPress PLUS to exit.");
     printf("\x1b[2;1HLeft joystick position:");
     printf("\x1b[4;1HRight joystick position:");
@@ -45,63 +50,44 @@ int main(int argc, char* argv[])
     // Main loop
     while(appletMainLoop())
     {
-        // Scan the gamepad. This should be done once for each frame
-        padUpdate(&pad);
+        // Clear console
+        consoleClear();
 
-        // padGetButtonsDown returns the set of buttons that have been
-        // newly pressed in this frame compared to the previous one
-        u64 kDown = padGetButtonsDown(&pad);
+        for(size_t i=0; i<ARRAY_SIZE(pads); i++) {
+            printf("==== %zu ====\n", i);
 
-        // padGetButtons returns the set of buttons that are currently pressed
-        u64 kHeld = padGetButtons(&pad);
+            PadState *pad = &pads[i];
+            // Scan the gamepad. This should be done once for each frame
+            padUpdate(pad);
 
-        // padGetButtonsUp returns the set of buttons that have been
-        // newly released in this frame compared to the previous one
-        u64 kUp = padGetButtonsUp(&pad);
+            // padGetButtonsDown returns the set of buttons that have been
+            // newly pressed in this frame compared to the previous one
+            u64 kDown = padGetButtonsDown(pad);
 
-        if (kDown & HidNpadButton_Plus)
-            break; // break in order to return to hbmenu
+            // padGetButtons returns the set of buttons that are currently pressed
+            u64 kHeld = padGetButtons(pad);
 
-        // Do the keys printing only if keys have changed
-        if (kDown != kDownOld || kHeld != kHeldOld || kUp != kUpOld)
-        {
-            // Clear console
-            consoleClear();
+            // padGetButtonsUp returns the set of buttons that have been
+            // newly released in this frame compared to the previous one
+            u64 kUp = padGetButtonsUp(pad);
 
-            // These two lines must be rewritten because we cleared the whole console
-            printf("\x1b[1;1HPress PLUS to exit.");
-            printf("\x1b[2;1HLeft stick position:");
-            printf("\x1b[4;1HRight stick position:");
+            if (kDown & HidNpadButton_Plus)
+                goto app_exit; // break in order to return to hbmenu
 
-            printf("\x1b[6;1H"); //Move the cursor to the sixth row because on the previous ones we'll write the joysticks' position
+            // Read the sticks' position
+            HidAnalogStickState analog_stick_l = padGetStickPos(pad, 0);
+            HidAnalogStickState analog_stick_r = padGetStickPos(pad, 1);
 
-            // Check if some of the keys are down, held or up
-            int i;
-            for (i = 0; i < 28; i++)
-            {
-                if (kDown & BIT(i)) printf("%s down\n", keysNames[i]);
-                if (kHeld & BIT(i)) printf("%s held\n", keysNames[i]);
-                if (kUp & BIT(i)) printf("%s up\n", keysNames[i]);
-            }
+            // Print the sticks' position
+            printf("LEFT: %04d; %04d\n", analog_stick_l.x, analog_stick_l.y);
+            printf("RIGHT: %04d; %04d\n", analog_stick_r.x, analog_stick_r.y);
         }
-
-        // Set keys old values for the next frame
-        kDownOld = kDown;
-        kHeldOld = kHeld;
-        kUpOld = kUp;
-
-        // Read the sticks' position
-        HidAnalogStickState analog_stick_l = padGetStickPos(&pad, 0);
-        HidAnalogStickState analog_stick_r = padGetStickPos(&pad, 1);
-
-        // Print the sticks' position
-        printf("\x1b[3;1H%04d; %04d", analog_stick_l.x, analog_stick_l.y);
-        printf("\x1b[5;1H%04d; %04d", analog_stick_r.x, analog_stick_r.y);
 
         // Update the console, sending a new frame to the display
         consoleUpdate(NULL);
     }
 
+app_exit:
     // Deinitialize and clean up resources used by the console (important!)
     consoleExit(NULL);
     return 0;
